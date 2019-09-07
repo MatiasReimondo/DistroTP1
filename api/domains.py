e474af894bc8dcf217e74e254b0af48c4b345819
MatiasRei <matiasreimondo@gmail.com>
2019-09-07 14:22:49 -0300

    Post custom domain

--- api/domains.py
+++ api/domains.py
@@ -3,8 +3,12 @@ from flask import abort, make_response
 from .resolver import Resolver
 from .domain import Domain
 
+# Data to serve with our API
+domains= {}
+
 
 # GET /domains/{domain}
+#Falta round robin
 def obtener_dominio(domain):
     resolver = Resolver()
     try:
@@ -15,3 +19,25 @@ def obtener_dominio(domain):
     response = make_response(result, 200)
     response.mimetype = 'application/json'
     return response
+
+# POST /api/custom-domain
+def agregar_dominio(**kwargs):
+    custom = kwargs.get('body')
+    domain = custom.get('domain')
+    ip = custom.get('ip')
+    if not domain or not ip:
+        return make_response({},400)
+
+    dup = False
+    for dominio_existente in domains.values():
+        dup = domain == json.loads(dominio_existente).get('domain')
+        if dup: break
+    if dup:
+        return make_response({},400)
+
+    new_custom = json.dumps(Domain(domain,ip,True).__dict__)
+    domains[domain] = new_custom
+    response = make_response(new_custom, 201)
+    response.mimetype = 'application/json'
+    return response
+
